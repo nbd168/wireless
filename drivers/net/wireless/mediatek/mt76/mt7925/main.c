8ffcd3032172a65ed12d3c58a9b0a36734fd1002
Sean Wang <sean.wang@mediatek.com>
2024-07-03 14:42:09 -0700

    wifi: mt76: mt7925: set mt7925_mcu_sta_key_tlv according to link id
    configure mt7925_mcu_sta_key_tlv according to link id
    
    We created a link id field in the common structure mt76_wcid so that
    mt7925_mcu_sta_key_tlv can access per-link STA/BSS. Additionally,
    .link_id will be required when reporting the link information of Rx status
    to mac80211. We will submit the changes for reporting the Rx status in
    a separate patch.
    
    Co-developed-by: Ming Yen Hsieh <mingyen.hsieh@mediatek.com>
    Signed-off-by: Ming Yen Hsieh <mingyen.hsieh@mediatek.com>
    Co-developed-by: Deren Wu <deren.wu@mediatek.com>
    Signed-off-by: Deren Wu <deren.wu@mediatek.com>
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Link: https://patch.msgid.link/2a6000f8e280ceacc4e19240ff41863426839776.1720042294.git.sean.wang@kernel.org
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7925/main.c
+++ drivers/net/wireless/mediatek/mt76/mt7925/main.c
@@ -746,6 +746,7 @@ static int mt7925_mac_link_sta_add(struct mt76_dev *mdev,
 	mlink->wcid.phy_idx = mvif->bss_conf.mt76.band_idx;
 	mlink->wcid.tx_info |= MT_WCID_TX_INFO_SET;
 	mlink->last_txs = jiffies;
+	mlink->wcid.link_id = link_sta->link_id;
 
 	ret = mt76_connac_pm_wake(&dev->mphy, &dev->pm);
 	if (ret)
